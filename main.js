b2c1f79f848724b8be4c12d8def58e535b09806f
SVJaashwin <88593780+SVJaashwin@users.noreply.github.com>
2022-04-05 11:02:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
-var canvas = document.getElementById("my_Canvas");
-ctx=canvas.getContext("2d");
-color="red";
+var canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
 
-canvas.addEventListener("mousedown",my_mouseDown);
+var mouseEvent = "Empty";
+var lastPositionX, lastPositionY;
 
-function my_mouseDown (e) {
-    color = document.getElementById("text_colour").value;
-    
-    radius = document.getElementById("text_Radius").value;
+var Radius = 50;
+var Colour = blue;
+var lineWidth = 1;
 
-    width = document.getElementById("text_width").value;
+canvas.addEventListener("mousedown", my_mouseDown);
 
-    mouseX= e.clientX-canvas.offsetLeft;
-    
-    mouseY= e.clientY-canvas.offsetTop;
-    console.log(e.clientX+" & e.clientY =" +e.clientY)  
-    console.log("offsetX = " +  canvas.offsetLeft + " & offsetY =" +canvas.offseTop);
-    console.log( "X = "+ mouseX  + " & Y =" + mouseY);
-    circle(mouseX,mouseY);
-} 
+function my_mouseDown(e) {
+    mouseEvent = "mousedown";
+    Radius = document.getElementById("Radius").value;
+    Colour = document.getElementById("Colour").value;
+    lineWidth = document.getElementById("line_width").value;
+}
 
-function my_mousemove (e) {
-    currentPostionX=e.clientX-canvas.offsetLeft;
-    
-    currentPostionY=e.clientY-canvas.offsetTop;
-    
-    
-    if ( mouseEvent == "mousedown"){
-    ctx.beginPath();
-    ctx.arcWidth=arcwidth;
-    ctx.strokeStyle=color;
-    ctx.moveTo(lastPositionX,lastPositionY);
-    ctx.arc(currentPostionX,currentPostionY,radius,0,2*Math.PI);
-    ctx.stroke();
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e) {
+    mouseEvent = "mouseup";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e) {
+    mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e) {
+    currentPostionX = e.clientX - canvas.offsetLeft;
+
+    currentPostionY = e.clientY - canvas.offsetTop;
+
+
+    if (mouseEvent == "mousedown") {
+        ctx.beginPath();
+        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = Colour;
+        ctx.arc(currentPostionX, currentPostionY, Radius, 0, 2 * Math.PI);
+        ctx.stroke();
     }
-    lastPositionX=currentPostionX;
-    lastPositionY=currentPostionY;
-    }
+    lastPositionX = currentPostionX;
+    lastPositionY = currentPostionY;
+}
